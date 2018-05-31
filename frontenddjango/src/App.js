dbf741b7a6303a21944453869898f52694ddee4c
rawalhome@gmail.com <rawalhome@gmail.com>
2018-05-30 23:44:56 -0500

    Front-end for Django..WIP

--- frontenddjango/src/App.js
+++ frontenddjango/src/App.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-const axios = require('axios');
+import axios from 'axios';
+// const axios = require('axios');
 
 
 const theUrl = 'http://127.0.0.1:8000/api/notes/';
-
-// const list = [
-//   {
-//       "title": "test",
-//       "content": "OK"
-//   },
-//   {
-//       "title": "hello, world",
-//       "content": "Hi!"
-//   },
-//   {
-//       "title": "test3",
-//       "content": "let's see if this works"
-//   }
-// ];
-
-// function httpGet(theUrl)
-// {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-//     xmlHttp.send( null );
-//     return xmlHttp.responseText;
-// }
 
 class App extends Component {
   // constructor(props){
@@ -40,30 +18,21 @@ class App extends Component {
 
  
 
-  async componentDidMount() {
-    try{
+  componentDidMount() {
+    // console.log("runs");
       axios.get(theUrl)
       .then(response => {
-        console.log("pUNIT", response)
-        this.setState( { notes } )
+        console.log("pUNIT", response.data)
+        this.setState( { notes: response.data } )
       })
     } catch(e) {
       console.log("errors galore", e);
     }
-  }
-
-    // try {
-    //   const res = await httpGet('http://127.0.0.1:8000/api/notes/');
-    //   console.log("33", res);
-    //   const notes = await res.json();
-    //   this.setState( { notes } );
-    // } catch (e) {
-    //   console.log("this is punit", e);
-    // }
   
 
-   
+    
   render() {
+    console.log(this.state.notes);
     return (
       <div className="App">
         <header className="App-header">
